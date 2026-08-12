11d8c7507baaa6a795e55b93ec8f076f666b8bfc
Tomas KOVAC <office@ztk-comp.sk>
2026-08-12 15:58:07 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 MQTT_USER = "gps_user"
 MQTT_PASS = "gps_host"
-MQTT_HOST = "mqtt.ztk-comp.sk"
+MQTT_HOST = "mqtt.server.xx"
 MQTT_PORT = 1883
 
-SSID = "iPhone14"
-PASS = "12345678901234567890123456"
-HOST = "gps.ztk-comp.sk"
+SSID = "NAME_SSID"
+PASS = "PASS_SSID"
+HOST = "firmware.server.xx"
