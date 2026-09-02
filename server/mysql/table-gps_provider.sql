ab3f6fd097209766fc95fe41c796d71360a836d4
Tomas KOVAC <office@ztk-comp.sk>
2026-09-02 16:48:17 +0200

    Update table-gps_provider.sql

--- server/mysql/table-gps_provider.sql
+++ server/mysql/table-gps_provider.sql
@@ -7,3 +7,15 @@ CREATE TABLE `gps_provider` (
   `ip` varchar(15) NOT NULL,
   `provider` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+
+--
+-- Indexes for table `gps_provider`
+--
+ALTER TABLE `gps_provider`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for table `gps_provider`
+--
+ALTER TABLE `gps_provider`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
