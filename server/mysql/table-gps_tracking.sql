06afd738e6591ed172a30cd8ccf158d324e5369a
Tomas KOVAC <office@ztk-comp.sk>
2026-09-02 16:48:44 +0200

    Update table-gps_tracking.sql

--- server/mysql/table-gps_tracking.sql
+++ server/mysql/table-gps_tracking.sql
@@ -26,3 +26,17 @@ CREATE TABLE `gps_tracking` (
   `temprpi` char(10) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
   `loadrpi` char(11) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+
+--
+-- Indexes for table `gps_tracking`
+--
+ALTER TABLE `gps_tracking`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`),
+  ADD KEY `idx_devicerpi_time` (`devicerpi`,`time`);
+
+--
+-- AUTO_INCREMENT for table `gps_tracking`
+--
+ALTER TABLE `gps_tracking`
+  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT;
