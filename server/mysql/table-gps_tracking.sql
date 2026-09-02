57fe0b5356770d99c8e4e1adf094f0a88bd8eb99
Tomas KOVAC <office@ztk-comp.sk>
2026-09-02 16:45:44 +0200

    Create table-gps_tracking.sql

---
+++ server/mysql/table-gps_tracking.sql
@@ -0,0 +1,28 @@
+--
+-- Table structure for table `gps_tracking`
+--
+
+CREATE TABLE `gps_tracking` (
+  `id` int(8) NOT NULL,
+  `lat` varchar(14) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL DEFAULT '',
+  `lon` varchar(14) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL DEFAULT '',
+  `alt` varchar(10) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL DEFAULT '',
+  `acc` varchar(10) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL DEFAULT '',
+  `spd` varchar(10) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL DEFAULT '',
+  `sat` varchar(2) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL DEFAULT '',
+  `time` varchar(26) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL DEFAULT '',
+  `bat` varchar(5) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL DEFAULT '',
+  `ip` varchar(40) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL DEFAULT '',
+  `year` varchar(4) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL DEFAULT '',
+  `month` char(2) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL DEFAULT '',
+  `day` char(2) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL DEFAULT '',
+  `hour` char(2) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL DEFAULT '',
+  `minute` char(2) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL DEFAULT '',
+  `second` char(2) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL DEFAULT '',
+  `device` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL DEFAULT '',
+  `provider` char(10) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `direction` char(10) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `devicerpi` char(16) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `temprpi` char(10) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `loadrpi` char(11) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
